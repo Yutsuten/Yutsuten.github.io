5f36c65a8e3756e73a1d879c6a0183f4097615f1
Mateus Etto <mateus.etto@gmail.com>
2017-05-09 17:11:56 -0300

    Created structure for experiences

---
+++ js/experience.js
@@ -0,0 +1,43 @@
+var experience = {
+  programming: {
+    language: {
+      javascript: { time: 0, skill: 0 },
+      csharp: { time: 0, skill: 0 },
+      html5: { time: 0, skill: 0 },
+      css: { time: 0, skill: 0 },
+      cplusplus: { time: 0, skill: 0 },
+      java: { time: 0, skill: 0 },
+      php: { time: 0, skill: 0 },
+    },
+    database: {
+      oracle: { time: 0, skill: 0 },
+      mysql: { time: 0, skill: 0 },
+      mongodb: { time: 0, skill: 0 },
+    },
+    testing: {
+      mocha: { time: 0, skill: 0 },
+    },
+    framework: {
+      angularjs: { time: 0, skill: 0 },
+      ionic: { time: 0, skill: 0 },
+      pixijs: { time: 0, skill: 0 },
+      stylus: { time: 0, skill: 0 },
+      pug: { time: 0, skill: 0 },
+    },
+    tool: {
+      unity: { time: 0, skill: 0 },
+      atom: { time: 0, skill: 0 },
+      visualStudio: { time: 0, skill: 0 },
+      git: { time: 0, skill: 0 },
+    },
+    paradigm: {
+      procedural: { time: 0, skill: 0 },
+      objectOriented: { time: 0, skill: 0 },
+    },
+  },
+  language: {
+    portuguese: { skill: 0 },
+    english: { skill: 0 },
+    japanese: { skill: 0 },
+  },
+}
