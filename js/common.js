3014f27aac57b9441515c2f162b2b65fe6e18bc9
Mateus Etto <mateus.etto@gmail.com>
2017-05-10 00:56:09 -0300

    Code organization

--- js/common.js
+++ js/common.js
@@ -30,7 +30,12 @@ function generateFormattedTime(totalTime) {
   // Generate months text
   var monthsText = '';
   if (months == 1) {
-    monthsText = '1 month';
+    if (years == 0) {
+      monthsText = '1m';
+    }
+    else {
+      monthsText = '1 month';
+    }
   }
   else if (months >= 2) {
     monthsText = months + ' months';
@@ -57,11 +62,11 @@ function appendArrayToHTML(array, divId, headersArray) {
   // Sort (descending) the array
   array.sort(function(a, b) {return b.time - a.time;});
 
-  // Get language with biggest experience
+  // Get the one with biggest experience
   if (array[0].time > biggestTime)
     biggestTime = array[0].time;
 
-  // Add the programming experiences to the html
+  // Add to the html
   var numSkillsToShow = (array.length <= MAX_SKILLS_TO_SHOW) ? array.length : MAX_SKILLS_TO_SHOW;
   for (var i = 0; i < numSkillsToShow; i++) {
     if (array[i].time > 0) {
@@ -77,42 +82,3 @@ function appendArrayToHTML(array, divId, headersArray) {
 
 var skillLvlText = ["", "Only Syntax", "Little Experience", "Used to it",
   "Experient", "Confident", "Very Confident", "Mastered"];
-
-var languageName = {
-  assembly: "Assembly",
-  javascript: "JavaScript",
-  csharp: "C#",
-  html5: "HTML5",
-  css: "CSS",
-  c: "C",
-  java: "Java",
-  php: "PHP",
-  lisp: "LISP",
-  prolog: "Prolog",
-};
-
-var frameworkName = {
-  angularjs: "AngularJS",
-  bootstrap: "Bootstrap",
-  jQuery: "jQuery",
-  ionic: "Ionic",
-  mocha: "Mocha",
-  pixijs: "PixiJS",
-  stylus: "Stylus",
-  pug: "Pug (jade)",
-  dotnet: ".NET",
-}
-
-var databaseName = {
-  oracle: "Oracle",
-  mysql: "MySQL",
-  mongodb: "MongoDB",
-}
-
-var toolName = {
-  unity: "Unity",
-  atom: "Atom",
-  visualStudio: "Visual Studio",
-  codeblocks: "Code Blocks",
-  git: "Git",
-}
---
+++ js/skillName.js
@@ -0,0 +1,38 @@
+var languageName = {
+  assembly: "Assembly",
+  javascript: "JavaScript",
+  csharp: "C#",
+  html5: "HTML5",
+  css: "CSS",
+  c: "C",
+  java: "Java",
+  php: "PHP",
+  lisp: "LISP",
+  prolog: "Prolog",
+};
+
+var frameworkName = {
+  angularjs: "AngularJS",
+  bootstrap: "Bootstrap",
+  jQuery: "jQuery",
+  ionic: "Ionic",
+  mocha: "Mocha",
+  pixijs: "PixiJS",
+  stylus: "Stylus",
+  pug: "Pug (jade)",
+  dotnet: ".NET",
+}
+
+var databaseName = {
+  oracle: "Oracle",
+  mysql: "MySQL",
+  mongodb: "MongoDB",
+}
+
+var toolName = {
+  unity: "Unity",
+  atom: "Atom",
+  visualStudio: "Visual Studio",
+  codeblocks: "Code Blocks",
+  git: "Git",
+}
