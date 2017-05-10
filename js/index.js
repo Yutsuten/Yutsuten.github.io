0045cd16442347022afcd6ae4a6fa26fe1dc7310
Mateus Etto <mateus.etto@gmail.com>
2017-05-09 23:51:11 -0300

    Generated experience bars from javascript db

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var MAX_SKILLS_TO_SHOW = 10;
+
 function generateSkillBars(languageName, expTimeBarPercentage, expTimeBarText, skillLvlBarPercentage, skillLvlBarText) {
   var htmlCode = '';
   htmlCode += '<div class="single-skill-container">';
@@ -12,4 +14,71 @@ function generateSkillBars(languageName, expTimeBarPercentage, expTimeBarText, s
   return htmlCode;
 }
 
-$('#skills-container').append(generateSkillBars("C#", 40, "2 years", 60, "Experienced"));
+function generateFormattedTime(totalTime) {
+  var years = Math.floor(totalTime / 12);
+  var months = totalTime % 12;
+
+  // Generate years text
+  var yearsText = '';
+  if (years == 1) {
+    yearsText = '1 year';
+  }
+  else if (years >= 2) {
+    yearsText = years + ' years';
+  }
+
+  // Generate months text
+  var monthsText = '';
+  if (months == 1) {
+    monthsText = '1 month';
+  }
+  else if (months >= 2) {
+    monthsText = months + ' months';
+  }
+
+  return yearsText + ' ' + monthsText;
+}
+
+var skillLvlText = ["", "Only Syntax", "Some Experience", "Used to it",
+  "Experient", "Confident", "Very Confident", "Mastered"];
+var languageName = {
+  assembly: "Assembly",
+  javascript: "JavaScript",
+  csharp: "C#",
+  html5: "HTML5",
+  css: "CSS",
+  c: "C",
+  java: "Java",
+  php: "PHP",
+  lisp: "LISP",
+  prolog: "Prolog",
+};
+
+// Create an array with the known programming languages
+var programmingLanguagesArray = [];
+for (key in experience.programming.language) {
+  if(experience.programming.language.hasOwnProperty(key)) {
+    var programmingLanguage = experience.programming.language[key];
+    programmingLanguage['language'] = languageName[key];
+    programmingLanguagesArray.push(programmingLanguage);
+  }
+}
+
+// Sort (descending) the array
+programmingLanguagesArray.sort(function(a, b) {return b.time - a.time;});
+
+// Get language with biggest experience
+var maxExpTime = programmingLanguagesArray[0].time;
+
+// Add the programming experiences to the html
+var numSkillsToShow = (programmingLanguagesArray.length <= MAX_SKILLS_TO_SHOW) ? programmingLanguagesArray.length : MAX_SKILLS_TO_SHOW;
+for (var i = 0; i < numSkillsToShow; i++) {
+  if (programmingLanguagesArray[i].time > 0) {
+    $('#skills-container').append(generateSkillBars(programmingLanguagesArray[i].language,
+      100 * programmingLanguagesArray[i].time / maxExpTime, generateFormattedTime(programmingLanguagesArray[i].time),
+      100 * programmingLanguagesArray[i].skill / 7, skillLvlText[programmingLanguagesArray[i].skill]));
+  }
+  else {
+    break;
+  }
+}
