7cf63883b47622a9abc6953d55708f7c75982f2c
Mateus Etto <mateus.etto@gmail.com>
2017-05-15 16:42:29 -0300

    Fix text outside container

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-var userLang = navigator.language || navigator.userLanguage;
-document.documentElement.lang = userLang;
-
 var options = $("#choose-language-container select option");
 for (var i = 0; i < options.length; i++) {
 	var elem = options[i];
 	if (navigator.language.indexOf(elem.value) >= 0){
+		document.documentElement.lang = elem.value;
 		elem.setAttribute('selected', 'true');
     break;
 	}
