3ee9b0fac1807981b0a94a29b189578eba6cff54
Mateus Etto <mateus.etto@gmail.com>
2018-04-21 23:25:01 +0900

    Notes show only chosen note

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -1,11 +1,6 @@
 $('#menu a').click(function(e) {
-  var href = $(this).attr('href');
-  if (href.charAt(0) == '/') {
-    $.get(href, function(data) {
-      $('#content').html(data);
-    });
-    return false;
-  }
+  $('div.note-content').hide();
+  $('div' + $(this).attr('href')).show();
 });
 
 $('#content').html($('#content').html().replace(/{% raw %}/g, '').replace(/{% endraw %}/g, ''));
