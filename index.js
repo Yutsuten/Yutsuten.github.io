c8e3c80ff9921664c905f6ee14a54ce5501af621
Mateus Etto <mateus.etto@gmail.com>
2018-07-25 21:25:12 +0900

    Remove jQuery, and use bootstrap-vue

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 require('./index.css');
 
 // Load global scripts
-global.jQuery = global.$ = require('jquery');
-global.Popper = require('popper.js');
-require('bootstrap');
 global.Vue.options.delimiters = ['[[', ']]'];
 
 // Load javascript files from assets
