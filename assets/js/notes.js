d99fdf9d11e89aca6fc44f116a10ef1d8ff7708c
Mateus Etto <mateus.etto@gmail.com>
2018-09-23 17:30:15 +0900

    Load notes titles into Javascript

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -5,12 +5,23 @@ if (location.pathname.indexOf('notes') !== -1) {
   new Vue({
     el: '#app',
     data: {
-      activeNote: window.location.href.split('#')[1]
+      activeNote: window.location.href.split('#')[1],
+      firstLevel: null,
+      secondLevel: null,
+      thirdLevel: null
     },
     methods: {
       seeNote: function (note) {
         this.activeNote = note
+      },
+      fetchInitialData: function () {
+        this.firstLevel = this.$refs.firstLevel.value
+        this.secondLevel = this.$refs.secondLevel.value
+        this.thirdLevel = this.$refs.thirdLevel.value
       }
+    },
+    mounted: function () {
+      this.fetchInitialData()
     }
   })
 }
