4f999c28dd5711401759879bfc6ab05f04798aa0
Mateus Etto <mateus.etto@gmail.com>
2019-06-19 21:25:15 +0900

    Working version without webpack

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,76 +1,71 @@
-import Vue from './Vue'
+new Vue({
+  el: '#app',
+  data: {
+    menu_class: 'menu',
+    profile_class: 'active',
+    education_class: '',
+    work_class: '',
+    skills_class: '',
+    contact_class: '',
+  },
+  methods: {
+    handleScroll: function () {
+      if (window.scrollY > 370) {
+        this.menu_class = 'menu affix'
+      } else {
+        this.menu_class = 'menu'
+      }
 
+      let elementsId = ['education', 'work-experience', 'skills', 'contact']
+      let elementsTop = {}
 
-if (location.pathname.indexOf('resume') !== -1) {
-  new Vue({
-    el: '#app',
-    data: {
-      menu_class: 'menu',
-      profile_class: 'active',
-      education_class: '',
-      work_class: '',
-      skills_class: '',
-      contact_class: '',
-    },
-    methods: {
-      handleScroll: function () {
-        if (window.scrollY > 370) {
-          this.menu_class = 'menu affix'
-        } else {
-          this.menu_class = 'menu'
-        }
+      // Find top of each element relative to the viewport
+      for (let i = 0; i < elementsId.length; i++) {
+        let elem = document.getElementById(elementsId[i])
+        let elemRect = elem.getBoundingClientRect()
+        elementsTop[elementsId[i]] = elemRect.top
+      }
 
-        let elementsId = ['education', 'work-experience', 'skills', 'contact']
-        let elementsTop = {}
-
-        // Find top of each element relative to the viewport
-        for (let i = 0; i < elementsId.length; i++) {
-          let elem = document.getElementById(elementsId[i])
-          let elemRect = elem.getBoundingClientRect()
-          elementsTop[elementsId[i]] = elemRect.top
-        }
-
-        // Find the biggest that doesn't surpass the threshold
-        let threshold = 20
-        let biggestTopElement = 'top'
-        let biggestTop = -999999
-        for (let elementId in elementsTop) {
-          let top = elementsTop[elementId]
-          if (top < threshold && top > biggestTop) {
-            biggestTop = top
-            biggestTopElement = elementId
-          }
-        }
-
-        this.profile_class = ''
-        this.education_class = ''
-        this.work_class = ''
-        this.skills_class = ''
-        this.contact_class = ''
-        switch (biggestTopElement) {
-          case 'top':
-            this.profile_class = 'active'
-            break
-          case 'education':
-            this.education_class = 'active'
-            break
-          case 'work-experience':
-            this.work_class = 'active'
-            break
-          case 'skills':
-            this.skills_class = 'active'
-            break
-          case 'contact':
-            this.contact_class = 'active'
-            break
+      // Find the biggest that doesn't surpass the threshold
+      let threshold = 20
+      let biggestTopElement = 'top'
+      let biggestTop = -999999
+      for (let elementId in elementsTop) {
+        let top = elementsTop[elementId]
+        if (top < threshold && top > biggestTop) {
+          biggestTop = top
+          biggestTopElement = elementId
         }
       }
-    },
-    created () {
-      document.addEventListener('scroll', this.handleScroll)
-    },
-    destroyed () {
-      document.removeEventListener('scroll', this.handleScroll)
+
+      this.profile_class = ''
+      this.education_class = ''
+      this.work_class = ''
+      this.skills_class = ''
+      this.contact_class = ''
+      switch (biggestTopElement) {
+        case 'top':
+          this.profile_class = 'active'
+          break
+        case 'education':
+          this.education_class = 'active'
+          break
+        case 'work-experience':
+          this.work_class = 'active'
+          break
+        case 'skills':
+          this.skills_class = 'active'
+          break
+        case 'contact':
+          this.contact_class = 'active'
+          break
+      }
     }
-  })
-}
+  },
+  created () {
+    document.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed () {
+    document.removeEventListener('scroll', this.handleScroll)
+  }
+})
