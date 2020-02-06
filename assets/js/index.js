b24ef547e0d910f648448aaddb43ecf31a2d9714
Yutsuten <mateus.etto@gmail.com>
2020-02-06 13:41:34 +0900

    Refactor javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,13 +13,26 @@ new Vue({
         this.$root.$emit('bv::toggle::collapse', 'index')
       }
     },
-    showMenu: function() {
+    showMenu: function () {
       if (!this.menuShown) {
         this.$root.$emit('bv::toggle::collapse', 'index')
       }
+    },
+    keydown: function (e) {
+      let ctrl = e.ctrlKey
+      let backspace = e.keyCode === 8
+      let slash = e.keyCode === 191
+      let letter = e.keyCode >= 65 && e.keyCode <= 90
+      if (!ctrl && (backspace || slash || letter)) {
+        document.getElementById('search').focus()
+      }
+    },
+    collapseStateChanged: function (collapseId, isJustShown) {
+      this.menuShown = isJustShown
     }
   },
   updated: function () {
+    // Update index in menu
     let ulElem
     for (let refName in this.$refs) {
       ulElem = this.$refs[refName]
@@ -31,7 +44,7 @@ new Vue({
         ulElem.previousElementSibling.style.display = ''
       }
     }
-    // Update URL with new search value
+    // Update URL with the new search value
     let url = new URL(window.location.href)
     if (this.search) {
       url.searchParams.set('search', this.search)
@@ -41,24 +54,10 @@ new Vue({
     window.history.replaceState({}, null, url.href)
   },
   mounted: function () {
-    let self = this
-    window.addEventListener('keydown', function (e) {
-      let ctrl = e.ctrlKey
-      let backspace = e.keyCode === 8
-      let slash = e.keyCode === 191
-      let letter = e.keyCode >= 65 && e.keyCode <= 90
-      if (!ctrl && (backspace || slash || letter)) {
-        document.getElementById('search').focus()
-      }
-    })
-    self.$root.$on('bv::collapse::state', function (collapseId, isJustShown) {
-      self.menuShown = isJustShown
-    })
+    window.addEventListener('keydown', this.keydown)
+    this.$root.$on('bv::collapse::state', this.collapseStateChanged)
 
-    let url = new URL(window.location.href)
-    let search = url.searchParams.get('search', '')
-    if (search) {
-      self.search = search
-    }
+    let search = new URL(window.location.href).searchParams.get('search')
+    this.search = search ? search : '';
   }
 })
